4a909d27ef6869c4dd78a518f25b7ef7ccb15dbe
Vishal-Kank <kankvishalkank@gmail.com>
2019-09-30 22:28:58 +0530

    Updates

---
+++ precache-manifest.e435ef3eb15f4ee4bf9e986c8d92b18f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9cf8dee5538d27b8361c517a1d6dff5",
+    "url": "/Post_Wall/index.html"
+  },
+  {
+    "revision": "81e2178ebebdcd9b23a4",
+    "url": "/Post_Wall/static/css/main.1ada9bd2.chunk.css"
+  },
+  {
+    "revision": "04bd7b84c00a0ac97d06",
+    "url": "/Post_Wall/static/js/2.cf2c5c94.chunk.js"
+  },
+  {
+    "revision": "81e2178ebebdcd9b23a4",
+    "url": "/Post_Wall/static/js/main.83d96bd6.chunk.js"
+  },
+  {
+    "revision": "efefb054b8afbed84085",
+    "url": "/Post_Wall/static/js/runtime-main.c3b79ae2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Post_Wall/static/media/logo.25bf045c.svg"
+  }
+]);
--- src/actions/types.js
+++
@@ -1,2 +0,0 @@
-export const FETCH_POSTS = 'FETCH_POST'; // action fetch data from API 
-export const NEW_POST = 'NEW_POST'; // action to post new data from form
